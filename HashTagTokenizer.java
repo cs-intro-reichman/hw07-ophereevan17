60f6da0bd9158e27f69afbebf26a0f75d940e911
ophereevan17 <ophere.evan@post.runi.ac.il>
2024-02-01 14:56:46 +0200

    HW_07

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -5,7 +5,9 @@ public class HashTagTokenizer {
 	public static void main(String[] args) {
 
 		String hashTag = args[0];
+		hashTag = hashTag.toLowerCase();
 		String []dictionary = readDictionary("dictionary.txt");
+		System.out.println (existInDictionary(hashTag, dictionary));
 		breakHashTag(hashTag, dictionary);
 	}
 
@@ -13,14 +15,19 @@ public class HashTagTokenizer {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-
-		// Your code here
-
+		for (int i = 0 ; i < 3000 ; i++){
+			dictionary[i] = in.readLine();
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for ( int i = 0; i < dictionary.length; i++){
+			if (word.equals(dictionary[i])){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
@@ -29,12 +36,25 @@ public class HashTagTokenizer {
         if (hashtag.isEmpty()) {
             return;
         }
- 
+
         int N = hashtag.length();
-
         for (int i = 1; i <= N; i++) {
-		
-        }
-    }
+			for (int j = 0; j < 3000; j++){
 
+				if(existInDictionary(hashtag.substring(0, i), dictionary)) {
+					System.out.println(hashtag.substring(0, i));
+
+					hashtag = hashtag.replace(hashtag.substring(0, i), "");
+					// Recursive call
+					breakHashTag(hashtag, dictionary);
+                
+					// Exits the inner loop after finding a match
+					return;
+				}
+			}
+		}
+	 
+		// Recursive call outside the inner loop
+		breakHashTag(hashtag, dictionary);
+	}
 }
